e28ddfc58d1803f22cead6c5e587374aeb67c8af
Chad Reynolds <chad.s.reynolds@gmail.com>
2020-10-14 16:38:05 -0400

    update

--- programming101/loops.py
+++ programming101/loops.py
@@ -1,4 +1,44 @@
-i = 0
-while i <10:
-    print(i)
-    i = i + 1
+# i = 0
+# while i <10:
+#     print(i)
+#     i = i + 1  #same as i =+ 1
+
+# i = 2
+# max_num = 100
+# while i < max_num:
+#     print(i)
+#     i += i
+
+# j = 0           #creates an infinite loop due to no limiting value
+# while j < 10:
+#     print(j)
+
+# name = "" #empty string is giving an empty value here, b/c variables must be given a value                  
+# while name != "clint":
+#     name = input("please say your name is clint\n")
+
+# print("Great I knew you could it.")
+
+# i = 1
+# while i < 11:
+#     print(i)
+#     i += 1
+
+# i = 10
+# while i > 0:
+#     print(i)
+#     i += -1
+
+
+username = '' 
+password = ''# something wrong with this, doesnt progress to password if correct username entered correctly first time.
+username = input('what is your username?\n')
+while username != 'chad':
+    username = input('wrong username\n')
+    if username == 'chad':
+        password = input('What is your password\n')
+        while password != '1234':
+            password = input('wrong password\n')
+            if password == '1234':
+                print('welcome, you guessed correctly.')
+
