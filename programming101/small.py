5fc0447de26f2f27539b3ebd44a1918961a4b3d6
Chad Reynolds <chad.s.reynolds@gmail.com>
2020-10-15 09:47:02 -0400

    added file

---
+++ programming101/small.py
@@ -0,0 +1,64 @@
+# #exercise 1
+# name = input("what's your name?\n")
+# print(f"Hello, {name}!")
+
+# #exercise 2 - convert all lowercase to uppercase, letters already uppercase remain uppercase
+# name = input("WHAT'S YOUR NAME?\n")
+# print(f"HELLO, {name.upper()}!")
+# print(f"YOUR NAME HAS {len(name)} LETTERS IN IT! AWESOME!")
+
+# #exercise 3
+# name = input("What is your name?\n")
+# subject = input("what is you favorite subject in school?\n")
+# print(f"{name}'s favorite subject in school is {subject}.")
+
+# #exercise 4
+# day = int(input("What day is it using (0-6) in place of the day?\n"))
+# if day == 0:
+#     print("Sunday")
+# elif day == 1:
+#     print("Monday")
+# elif day == 2:
+#     print("Tuesday")
+# elif day == 3:
+#     print("Wendsday")
+# elif day == 4:
+#     print("Thursday")
+# elif day == 5:
+#     print("Friday")
+# elif day == 6:
+#     print("Saturday")
+
+#Exercise 5
+# day = int(input("What day is it using (0-6) in place of the day?\n"))
+# if day == 0:
+#     print("Sleep in")
+# elif day == 6:
+#     print("Sleep in")
+# elif day >= 1 or 2 or 3 or 4 or 5:
+#     print("GO TO WORK!")
+
+# #exercise 6
+# celsius = int(input("What is temp in C? "))
+# fahr = (celsius * 9/5) + 32
+# print("%i F" % (fahr))
+# # print(int(temp * 9/5 + 32) + str(F))  how to get this to concatenate correctly?
+
+# #exercise 7
+# i = 1
+# while i < 11:
+#     print(i)
+#     i += 1
+
+# #exercise 8
+# start = int(input("Start from: "))
+# end = int(input("End on: "))
+# while start <= end:
+#     print(start)
+#     start += 1
+
+# #exercise 9 - - prob needed a 'while' loop here
+# print("*****\n*****\n*****\n*****\n*****\n")
+
+#exercise 10 - create a X * X size square of * at input of a number
+size = int(input("How big is the square? "))
